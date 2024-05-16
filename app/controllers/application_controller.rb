baa97a9b46145684194bec69b67e31a2bbeb05a9
Ryo Yokoyama <yokoyamaryou@vec24v003.local>
2024-05-16 17:13:30 +0900

    フレンドリーフォワーディングの実装を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,16 @@
 class ApplicationController < ActionController::Base
-  protected
+  before_action :storre_location
+
+  def storre_location
+    if request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+      store_location_for(:user, request.fullpath)
+    end
+  end
 
   def after_sign_in_path_for(resource)
     path = stored_location_for(resource) || root_path
     logger.info "after_sign_in_path: #{path}"
     flash[:notice] = "ログインに成功しました。"
     path
-  end
-
-  before_action :store_user_location!, if: :storable_location?
-
-  private
-
-  def storable_location?
-    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
-  end
-
-  def store_user_location!
-    store_location_for(:user, request.fullpath)
   end
 end
