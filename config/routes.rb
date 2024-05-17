6b8085869e9676b784f457d1c5d51427362869b4
Ryo Yokoyama <yokoyamaryou@vec24v003.local>
2024-05-17 18:58:26 +0900

    execute: rails generate devise:controllers users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root "hello#index"
   # サインアウトのルートを追加
   devise_scope :user do
