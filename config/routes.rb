bdedf9287bdb670207bbcc476eac9389f83326ef
Ryo Yokoyama <yokoyamaryou@vec24v003.local>
2024-05-23 13:43:26 +0900

    コメント削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "hello#index"
-  # サインアウトのルートを追加
+  
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
